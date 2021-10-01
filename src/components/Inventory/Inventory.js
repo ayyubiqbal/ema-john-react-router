1e307796b9a1b638ec981b7077a70d1fd934089f
Ayyub Iqbal <abuayubiqbal@gmail.com>
2021-10-01 23:34:18 +0600

    test lorem

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -4,8 +4,8 @@ const Inventory = () => {
     return (
         <div>
             <h2>This is Inventory</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis quo dignissimos a itaque libero quos necessitatibus in, voluptatibus ducimus dolor!</p>
         </div>
     );
-};
-
+}
 export default Inventory;
